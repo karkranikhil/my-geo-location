3a5f86eefc2e35443666c0de77301610ba4120c4
nikhil karkra <karkra.nikhil@gmail.com>
2019-03-16 20:33:13 +1100

    mongoose model for user/pin

--- server.js
+++ server.js
@@ -2,6 +2,15 @@ const {ApolloServer} = require('apollo-server')
 
 const typeDefs=require('./typeDefs')
 const resolvers=require('./resolvers')
+const mongoose = require('mongoose')
+require('dotenv').config()
+console.log(process.env.MONGO_URI)
+var localUri = 'mongodb://localhost/karkra-geo-location'
+mongoose.connect(localUri,{ useNewUrlParser: true })
+.then(()=>console.log('DB connected'))
+.catch((err)=>console.error(err))
+
+
 const server = new ApolloServer({
     typeDefs:typeDefs,
     resolvers:resolvers
