8bc914d47c80fd28e6c7ef632c997b46673e1cd5
nikhil karkra <karkra.nikhil@gmail.com>
2019-03-17 00:27:29 +1100

    setting up google Auth 2.0

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -1,9 +1,22 @@
 import React from "react";
+import {GoogleLogin} from 'react-google-login'
 import { withStyles } from "@material-ui/core/styles";
 // import Typography from "@material-ui/core/Typography";
 
 const Login = ({ classes }) => {
-  return <div>Login</div>;
+  const onSuccess=googleUser=>{
+    const idToken = googleUser.getAuthResponse().id_token
+    console.log(idToken)
+  }
+  const onFailure=err=>{
+    console.log({err})
+  }
+  return <GoogleLogin
+  clientId="665865701772-h38bchhr4lefc1mjnt10c3qtn8g29obv.apps.googleusercontent.com"
+  onSuccess={onSuccess}
+  onFailure ={onFailure}
+  isSignedIn={true}
+  />
 };
 
 const styles = {
