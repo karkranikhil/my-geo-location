8cabd0f9d6385bd4d00d8abc15b97a21ff6bcd18
nikhil karkra <karkra.nikhil@gmail.com>
2019-03-19 22:42:36 +1100

    state management using useReducer and context

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -1,40 +1,51 @@
-import React from "react";
+import React, {useContext} from "react";
 import {GraphQLClient} from 'graphql-request'
 import {GoogleLogin} from 'react-google-login'
 import { withStyles } from "@material-ui/core/styles";
-// import Typography from "@material-ui/core/Typography";
-
-const ME_QUERY=`
-{
-  me{
-    _id
-    name
-    picture
-    email
-  }
-}
-`
+import Typography from "@material-ui/core/Typography";
+import Context from '../../context'
+import { dark } from "@material-ui/core/styles/createPalette";
+import {ME_QUERY} from '../../graphql/queries'
 
 const Login = ({ classes }) => {
+  const {dispatch} = useContext(Context)
   const onSuccess= async googleUser=>{
-    const idToken =  googleUser.getAuthResponse().id_token
-    console.log({idToken})
-    const client = new GraphQLClient('http://localhost:4000/graphql',
-    {
-      headers:{authorization:idToken}
-    })
-    const data = await client.request(ME_QUERY)
-    console.log({data})
+    try{
+      const idToken =  googleUser.getAuthResponse().id_token
+      const client = new GraphQLClient('http://localhost:4000/graphql',
+      {
+        headers:{authorization:idToken}
+      })
+      const {me} = await client.request(ME_QUERY)
+      console.log(me)
+      dispatch ({type:'LOGIN_USER', payload:me})
+    } catch(err){
+      onFailure(err)
+    }
+    
   }
   const onFailure=err=>{
-    console.log({err})
+    console.error("Error logging in ", err)
   }
-  return <GoogleLogin
-  clientId="665865701772-h38bchhr4lefc1mjnt10c3qtn8g29obv.apps.googleusercontent.com"
-  onSuccess={onSuccess}
-  onFailure ={onFailure}
-  isSignedIn={true}
-  />
+  return (
+    <div className={classes.root}>
+    <Typography
+      component="h1"
+      variant="h3"
+      gutterbutton ="true"
+      noWrap
+      style={{color:"rgb(66, 133, 244)"}}>Welcome</Typography>
+        <GoogleLogin
+      clientId="665865701772-h38bchhr4lefc1mjnt10c3qtn8g29obv.apps.googleusercontent.com"
+      onSuccess={onSuccess}
+      onFailure ={onFailure}
+      isSignedIn={true}
+      theme="dark"
+      />
+    </div>
+  )
+  
+  
 };
 
 const styles = {
