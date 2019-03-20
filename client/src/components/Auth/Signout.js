cf89f6d4e963a51d6d01fc04f70af50132af320d
nikhil karkra <karkra.nikhil@gmail.com>
2019-03-21 00:00:07 +1100

    header and signout

--- client/src/components/Auth/Signout.js
+++ client/src/components/Auth/Signout.js
@@ -1,10 +1,32 @@
-import React from "react";
+import React, {useContext} from "react";
 import { withStyles } from "@material-ui/core/styles";
-// import ExitToApp from "@material-ui/icons/ExitToApp";
-// import Typography from "@material-ui/core/Typography";
-
+import {GoogleLogout} from 'react-google-login'
+import ExitToAppIcon from "@material-ui/icons/ExitToApp";
+import Typography from "@material-ui/core/Typography";
+import Context from '../../context'
 const Signout = ({ classes }) => {
-  return <div>Signout</div>;
+const {dispatch}  = useContext(Context)
+const onSignout =()=>{
+  dispatch({type:'SIGNOUT_USER'})
+  console.log("signed out user")
+}
+  return (
+  <GoogleLogout
+  onLogoutSuccess={onSignout}
+  render={({onClick})=>(
+    <span className={classes.root} onClick={onClick}>
+    <Typography
+    variant="body1"
+    className={classes.buttonText}
+    >
+
+      Signout
+    </Typography>
+    <ExitToAppIcon className={classes.buttonIcon}/>
+    </span>
+  )}
+  
+  />)
 };
 
 const styles = {
@@ -13,11 +35,11 @@ const styles = {
     display: "flex"
   },
   buttonText: {
-    color: "orange"
+    color: "white"
   },
   buttonIcon: {
     marginLeft: "5px",
-    color: "orange"
+    color: "white"
   }
 };
 
